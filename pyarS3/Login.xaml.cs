6db9e0f8e8bd484ceb23f489aa1545aa96462e54
PabloYar012885 <pyar@esmeraldafarms.com>
2022-12-13 19:36:28 -0500

    Agregar archivos de proyecto.

---
+++ pyarS3/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pyarS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "pablo";
+            string clave = "12345";
+
+            if (usuario == txtUsuario.Text && clave == txtClave.Text)
+            {
+                Navigation.PushAsync(new Registro(txtUsuario.Text));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario o clave incorrecta", "Cerrar");
+            }
+
+        }
+    }
+}
